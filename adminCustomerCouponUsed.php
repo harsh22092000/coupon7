43764847471b9e7d1b18289493af3ac992056bea
tejani harsh <tejani317@gmail.com>
2021-04-30 13:37:13 +0530

    Completed Raw

--- adminCustomerCouponUsed.php
+++ adminCustomerCouponUsed.php
@@ -2,23 +2,45 @@
 <?php include './index.php'; ?>
 <?php include './connection.php';?>
 <div class="content-wrapper" style="padding: 30px;">
-        <link href="//cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css"/>
+        <!-- <link href="//cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.0/css/jquery.dataTables.css">
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script type="text/javascript" src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script> -->
         <!--fads-->
+
+
+
+        <!-- pdf -->
         
-        <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.0/jquery.dataTables.min.js"></script>
+        <link rel="stylesheet" href="plugins\datatables\jquery.dataTables.min.css">
+        <link rel="stylesheet" href="plugins\datatables\buttons.dataTables.min.css">
+        
+        <script src="./plugins/jquery/jquery.min.js"></script>
+         <script src="./plugins/Others/datatables/jquery.dataTables.min.js"></script>
+<script src="./plugins/Others/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="./plugins/Others/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="./plugins/Others/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="./plugins/Others/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="./plugins/Others/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="./plugins/Others/jszip/jszip.min.js"></script>
+<script src="./plugins/Others/pdfmake/pdfmake.min.js"></script>
+<script src="./plugins/Others/pdfmake/vfs_fonts.js"></script>
+<script src="./plugins/Others/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="./plugins/Others/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="./plugins/Others/datatables-buttons/js/buttons.colVis.min.js"></script>
+        <!-- pdf -->
+        <!-- <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.0/jquery.dataTables.min.js"></script> -->
         <style>
             .action{
                 width: 70px;
             }
         </style>
         <center><h1 id="reportHead">Redeemed Coupons</h1></center>
+        <span class="exportas"> </span>
         <table id="example" class="display" style="width:100%">
         <thead>
            
@@ -69,10 +91,13 @@
     </table>
        
 <script type="text/javascript">
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
 
+$(document).ready(function() {
+    $("#example").DataTable({
+        
+      "buttons": ["copy", "csv", "pdf", "print"]
+    }).buttons().container().appendTo('.exportas');
+} );
 
 </script>
 </div>
--- content.php
+++ content.php
@@ -44,8 +44,8 @@ $noofAC=$rows3["count(*)"];
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#"></a></li>
-              <li class="breadcrumb-item active"></li>
+              <li class="breadcrumb-item"><a href="#">Admin Panel</a></li>
+              <li class="breadcrumb-item active">Reports</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
